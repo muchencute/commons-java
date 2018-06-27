5dfc673881f5d9a1a3e94a8a68022e1c38d11c29
米国梁 <nothingmi@muchencute.com>
2018-06-27 11:58:21 +0800

    增加对请求中查询参数的模式匹配

--- src/main/java/com/muchencute/commons/sparkjava/RouteUtils.java
+++ src/main/java/com/muchencute/commons/sparkjava/RouteUtils.java
@@ -1,13 +1,17 @@
 package com.muchencute.commons.sparkjava;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class RouteUtils {
 
     /**
      * 对于 URL 中包含 : 参数的路由与真实 Path 之间进行匹配
+     *
      * @param route 原始路由
-     * @param path 真实请求 Path 部分
+     * @param path  真实请求 Path 部分
      * @return 匹配返回 true, 不匹配返回 false
      */
     public static boolean matchParameterizedRoute(String route, String path) {
@@ -23,6 +27,28 @@ public class RouteUtils {
             String routeToken = routeTokenizer.nextToken();
             String pathToken = pathTokenizer.nextToken();
             if (!routeToken.startsWith(":") && !routeToken.equals(pathToken)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 判断实际请求的查询参数是否满足路由的定义
+     *
+     * @param route              原始路由
+     * @param requestQueryParams 实际请求的查询参数
+     * @return 满足则返回 true，不满足则返回 false
+     */
+    public static boolean matchQueryParameters(String route, Set<String> requestQueryParams) {
+
+        final String rawRouteQueryParameters = StringUtils.substringAfter(route, "?");
+        StringTokenizer routeTokenizer = new StringTokenizer(rawRouteQueryParameters, "&");
+
+        while (routeTokenizer.hasMoreTokens()) {
+            String routeToken = routeTokenizer.nextToken();
+            if (!requestQueryParams.contains(routeToken)) {
                 return false;
             }
         }
--- src/test/java/com/muchencute/commons/sparkjava/RouteUtilsTest.java
+++ src/test/java/com/muchencute/commons/sparkjava/RouteUtilsTest.java
@@ -3,6 +3,8 @@ package com.muchencute.commons.sparkjava;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import java.util.HashSet;
+
 public class RouteUtilsTest extends TestCase {
 
     public void testMatchParameterizedRoute() {
@@ -17,5 +19,26 @@ public class RouteUtilsTest extends TestCase {
         path = "/api/v1/permissionuser";
         Assert.assertTrue(RouteUtils.matchParameterizedRoute(route, path));
 
+        route = "patch./api/v1/permissionrole/:roleName?status";
+        path = "patch./api/v1/permissionrole/管理员?status=removed";
+        Assert.assertTrue(RouteUtils.matchParameterizedRoute(route, path));
+    }
+
+    public void testMatchQueryParameters() {
+
+        String route = "patch./api/v1/permissionrole/:roleName?status";
+        Assert.assertTrue(RouteUtils.matchQueryParameters(route, new HashSet<String>() {{
+            add("roleName");
+            add("status");
+        }}));
+
+        route = "patch./api/v1/permissionrole?roleName&status";
+        Assert.assertTrue(RouteUtils.matchQueryParameters(route, new HashSet<String>() {{
+            add("roleName");
+            add("status");
+        }}));
+        Assert.assertFalse(RouteUtils.matchQueryParameters(route, new HashSet<String>() {{
+            add("roleName");
+        }}));
     }
 }

7e06b8416edb67e3b7663c1ca76bd9f6a04186bf
米国梁 <nothingmi@muchencute.com>
2018-10-16 16:17:39 +0800

    补充电话号码正则表达式

--- src/test/java/com/muchencute/commons/regexp/RegexpUtilsTest.java
+++ src/test/java/com/muchencute/commons/regexp/RegexpUtilsTest.java
@@ -24,4 +24,11 @@ public class RegexpUtilsTest extends TestCase {
 
         Assert.assertTrue(Pattern.matches(EMAIL, "nothingmi@muchencute.com"));
     }
+
+    public void testPhone() {
+
+        Assert.assertTrue(Pattern.matches(PHONE, "+8618620398354"));
+        Assert.assertTrue(Pattern.matches(PHONE, "+86-18620398354"));
+        Assert.assertTrue(Pattern.matches(PHONE, "+86-731-8993-2530"));
+    }
 }
